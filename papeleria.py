1c15e02791dd92c45481cc6b3a8a84e55839d056
TomasCv0628 <tomas2224508@uis.edu.co>
2022-05-02 07:49:30 -0500

    Desarrollo: quiz papeleria. Diagrama  y codigo. TomasCv0628

--- papeleria.py
+++ papeleria.py
@@ -12,7 +12,7 @@ X =int(input("\nPrecio producto: "))
 
 if X < 3000:
     msj = (f"EL precio de venta es igual a: {X + X*15/100}")
-elif 3000 < X < 6000:
+elif 3000 < X <= 6000:
     msj = (f"EL precio de venta es igual a: {X + 500}")
 else:
     msj = (f"EL precio de venta es igual a: {X + X*25/100}")
